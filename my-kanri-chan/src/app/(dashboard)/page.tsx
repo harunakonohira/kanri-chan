8be93d84eb1ca5c191b9655e92b5d3e46e25e5cc
harunakonohira <harunakonohira613@gmail.com>
2026-05-31 17:36:53 +0900

    タスク登録モーダルを作成

--- my-kanri-chan/src/app/(dashboard)/page.tsx
+++ my-kanri-chan/src/app/(dashboard)/page.tsx
@@ -1,10 +1,27 @@
+'use client'
+
 // import Image from "next/image";
-// import styles from './page.module.css';
+import styles from '../page.module.css';
+import TaskModal from '@/components/ui/TaskModal';
+import Button from '@/components/ui/Button';
+import { useState } from 'react';
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+  const showTaskModal = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <>
+    <div className={styles.taskButton}></div>
+      <Button text='+ タスクを追加' onClick={showTaskModal}></Button>
+      <TaskModal
+        text='新規タスク登録'
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        buttonText='登録する'
+      />
     </>
   );
 }
